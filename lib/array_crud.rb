30750aa3e372b393b5ddebfcd90b6bb394ec88bc
Marcus Moreno <legendary.1198@gmail.com>
2020-07-08 05:10:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["I", "am", "really", "learning"]
+  array = ["am", "really", "learning", "4"]
 end
 
 def add_element_to_end_of_array(array, element)
